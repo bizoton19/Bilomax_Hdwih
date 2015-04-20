553b1cca3af4e67ad38b247886c9397a372c7f9c
Alexandre Salomon <bizoton19@gmail.com>
2015-04-19 22:03:15 -0400

    Job Posting Entites Added

--- Bilomax.Jobs.Domain.Core/JobPosting.cs
+++ Bilomax.Jobs.Domain.Core/JobPosting.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Bilomax.Generic.Infrastructure.Domain;
 
 namespace Hdwih.Jobs.Domain.Core
 {
-  public  class JobPosting
-    {
+  public  class JobPosting: EntityBase<int>, IAggregateRoot
+  {
       public int PostingId { get; set; }
       JobPosting Title { get; set; }
      
       PostingDetails PostingDetails { get; set; }
-    }
+
+      protected override void Validate()
+      {
+          throw new NotImplementedException();
+      }
+  }
 }
--- Bilomax.Jobs.Domain.Core/Organization.cs
+++ Bilomax.Jobs.Domain.Core/Organization.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Bilomax.Generic.Infrastructure.Domain;
 
 namespace Hdwih.Jobs.Domain.Core
 {
-   public class Organization
-    {
+   public class Organization : EntityBase<int>, IAggregateRoot
+   {
+       private IList<JobPosting> _postings;
+       public Organization()
+       {
+           _postings = new List<JobPosting>();
+       }
+       
         public string Name { get; set; }
         public Address Address { get; set; }
-        
+
+       public IEnumerable<JobPosting> GetAllJobPostings()
+       {
+           return new List<JobPosting>();
+       }
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
